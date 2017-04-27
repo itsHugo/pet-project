1ea6066ad26e17566c9e7e0031f781e1309d5bc8
Thai-Vu Nguyen <thaivu22@gmail.com>
2017-04-27 12:35:41 -0400

    For now, POST Request to Rest don't work

--- lib/requestHelper.ts
+++ lib/requestHelper.ts
@@ -26,3 +26,26 @@ export function _GetRequest(uri: string){
         })
     });
 }
+
+export function _PostRequest (uri: string, data){
+    return new Promise((resolve, reject) => {
+        var body = "";
+        console.log (JSON.stringify(data));
+        request.post(uri, { 
+                json: true,
+                body:  JSON.stringify(data) })
+        .on("data", function (chunck){
+            console.log(chunck)
+            body += chunck;
+        })
+        .on('end', function(response){
+            let jsonObject = JSON.parse(body);
+            //console.log(body);
+            console.log (jsonObject);
+            resolve(jsonObject);
+        }).on("error", function(err){
+            console.log(err);
+            reject(err);
+        })
+    })
+}
